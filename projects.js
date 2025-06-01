bf75779b8de90e43a226810ecb387b45af2c7f4d
iqbolshoh <iilhomjonbov777@gmail.com>
2025-06-01 18:32:34 +0500

    commit

---
+++ projects.js
@@ -0,0 +1,29 @@
+const projects = [
+    {
+        id: 1,
+        title: "Ninja O‘yini",
+        image: "rasmcha",
+        alt: "ninja jpg",
+        description: "Ninja o‘yinida foydalanuvchilarga raqobat muhiti taqdim etadi. Unda murakkab JavaScript algoritmlari ishlatilgan bo‘lib, o‘yin mantiqi, harakatlar aniqligi. O‘yin interfeysi nafis CSS texnikalari bilan bezatilgan. Vizual uyg‘unlik, animatsiyalar va mobil moslashuvchanlik o‘yinga yanada joziba bag‘ishlaydi. Foydalanuvchilar yangi o‘yinlar yaratishi, do‘stlarini raqobatga chorlab, ko‘proq ochko yig‘ish uchun bellashishlari mumkin. Bu o‘yinga ijtimoiylik va raqobat ruhini olib kiradi.",
+        technologies: ["CSS", "JavaScript", "Bootstrap", "PHP", "MySql"],
+        link: "#ninjaText"
+    },
+    {
+        id: 2,
+        title: "X vs O",
+        image: "img/x-o.png",
+        alt: "x vs o jpg",
+        description: "X vs O - bu klassik Tic Tac Toe o‘yinining zamonaviy veb-versiyasi bo‘lib, foydalanuvchilarga do‘st bilan yoki sun’iy intellektga qarshi bahs olib borish imkonini beradi. O‘yinda foydalanuvchi sun’iy intellekt darajasini (kuchsiz, o‘rtacha, kuchli) tanlashi mumkin, bu esa o‘yin jarayonini turli darajadagi qiyinchiliklar bilan boyitadi. Interfeys zamonaviy CSS dizayni asosida ishlangan. O‘yin barcha qurilmalarda qulay ishlash uchun moslashtirilgan. Bu loyiha sun’iy intellekt algoritmlari, foydalanuvchi tajribasi va dizayn uyg‘unligining ajoyib namunasi bo‘lib, dasturlash va interaktivlikni birlashtirgan holda, o‘yin orqali raqobat ruhini kuchaytiradi.",
+        technologies: ["Html", "JavaScript", "CSS"],
+        link: "https://gamee-x-vs-o.netlify.app/"
+    },
+    {
+        id: 3,
+        title: "Anon1m",
+        image: "img/anonim_2.jpg",
+        alt: "anonim jpg",
+        description: "Anonim – bu zamonaviy veb-texnologiyalar asosida yaratilgan onlayn musiqiy platforma bo‘lib, foydalanuvchilarga rep janridagi audio treklarni tinglash, yuklab olish va ulardan zavq olish imkoniyatini taqdim etadi. Platformada, asosan, Anon1M taxallusi ostida ijod qiluvchi ijodkorning musiqiy ishlari jamlangan bo‘lib, har bir trek o‘ziga xos kayfiyat va g‘oyani aks ettiradi. Sayt soddaligi bilan birga zamonaviy dizayn elementlarini ham o‘zida mujassam etgan. Foydalanuvchi interfeysi intuitiv va qulay bo‘lib, har bir mehmon uchun musiqani ajoyib va yoqimli qiladi. Anonim – bu musiqani his qilishni istaganlar uchun ochilgan eshikdir. U ijodkorlik, texnologiya va estetikani birlashtirgan holda, yangi avlod onlayn musiqiy loyihalarining yorqin namunasi hisoblanadi.",
+        technologies: ["HTML", "CSS", "JavaScript", "PHP", "MySql"],
+        link: "#anon1mText"
+    }
+];
